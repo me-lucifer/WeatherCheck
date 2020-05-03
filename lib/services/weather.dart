05bd9433ee24d9bd7643624225fd99931355e90c
Amit Mhaske <itsthelucifer@gmail.com>
2020-05-03 21:04:46 +0530

    Project Completed

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,28 @@
+import 'package:weathercheck/services/location.dart';
+import 'package:weathercheck/services/networking.dart';
+
+const apiKey = "ea14ee1ea93ea3923324039ff05c3a75";
+const openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather";
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        "$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric");
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        "$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric");
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
